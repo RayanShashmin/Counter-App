539deffa9fe3be1738456a6f4f58e9069356d90b
RayanShashmin <132475422+RayanShashmin@users.noreply.github.com>
2023-05-05 15:24:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [isEven, setIsEven] = useState(false);
+
+    useEffect(() => {
+    setIsEven(count % 2 === 0);
+    }, 
+    [count]);
+
 
   function handleIncrement() {
     setCount(count + 1);
@@ -19,6 +26,7 @@ function App() {
     <div className="counter-container">
       <h1 className="counter-title">Counter App</h1>
       <div className="count-display">{count}</div>
+      <div className="count-dis">{isEven ? 'Even' : 'Odd'}</div>
       <div className="button-container">
         <button className="increment-button" onClick={handleIncrement}>+</button>
         <button className="decrement-button" onClick={handleDecrement}>-</button>
